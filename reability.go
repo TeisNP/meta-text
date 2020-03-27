013126203cfcfcd0bbf60425a130032c21dde13e
teis <teis.n.p@gmail.com>
2020-03-27 10:43:52 +0100

    added readability index switch function

--- reability.go
+++ reability.go
@@ -1,10 +1,25 @@
 package metatext
 
+import "fmt"
+
+func (metaData *MetaData) CalculateReadabilityIndex(index int) (int, error) {
+	switch index {
+	case 0:
+		return metaData.CalculateLix(), nil
+	case 1:
+		return metaData.CalculateFleschReading(), nil
+	case 2:
+		return metaData.CalculateFleschGrade(), nil
+	default:
+		return 0, fmt.Errorf("Readbility index calculation not supported for case: %d", index)
+	}
+}
+
 func (metaData *MetaData) CalculateLix() int {
 	return metaData.WordCount/metaData.PeriodCount + (metaData.LongWordCount*100)/metaData.WordCount
 }
 
-func (metaData *MetaData) CalculateFleschReading(wordCount int, sentenceCount int, syllableCount int) int {
+func (metaData *MetaData) CalculateFleschReading() int {
 	return int(206.835 - 1.015*float64((metaData.WordCount/metaData.PeriodCount)) - 84.6*float64((metaData.SyllableCount/metaData.WordCount)))
 }
 
